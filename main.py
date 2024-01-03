d29ec221161b2f36d7edaa8db14e865a06ce4ea9
nyx-star <mamanyx256@gmail.com>
2024-01-02 20:16:53 -0600

    pycharm fixed

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-import deck
+import input_processor
 
-
-d = deck.deck()
-
-
-while True:
-    inpt = input("next move?")
-
-    if inpt == "draw":
-        d.draw()
-        d.show()
-    if inpt.startswith("play"):
-        cardnumber = int(inpt[5:])
-        d.play(cardnumber)
-        d.show()
-    else:
-        print(inpt + " is not a valid command")
+sc = input_processor.input_processor()
+sc.input_loop()
